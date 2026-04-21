0beebda4e42ac951ec7293a3b1d3ad32c1e096f5
lorenzo04andreoli <lorenzo04andreoli@gmail.com>
2026-04-20 22:21:56 -0300

    feat: Criando Controller

---
+++ src/main/java/com/lorenzoandreoli/api/controller/UsuarioController.java
@@ -0,0 +1,38 @@
+package com.lorenzoandreoli.api.controller;
+
+import com.lorenzoandreoli.api.model.Usuario;
+import com.lorenzoandreoli.api.service.UsuarioService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService service;
+
+    public UsuarioController(UsuarioService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Usuario> listar() {
+        return service.listar();
+    }
+
+    @PostMapping
+    public Usuario criar(@RequestBody Usuario usuario) {
+        return service.salvar(usuario);
+    }
+
+    @GetMapping("/{id}")
+    public Usuario buscar(@PathVariable Long id) {
+        return service.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        service.deletar(id);
+    }
+}
